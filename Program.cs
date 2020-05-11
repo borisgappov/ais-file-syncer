2d4a79ddc5cc9acebc5e10246284941857e13087
Boris <gappov@gmail.com>
2020-05-11 16:02:34 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using WebWindows.Blazor;
+using System;
+
+namespace AisFileSyncer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ComponentsDesktop.Run<Startup>("Ais File Syncer", "wwwroot/index.html");
+        }
+    }
+}
